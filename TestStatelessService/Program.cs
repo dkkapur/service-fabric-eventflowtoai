0dcd8a7cc3428e90703098db88c3026c3521b950
Deep Kapur <dekapur@microsoft.com>
2017-04-13 16:41:11 -0700

    initial commit

---
+++ TestStatelessService/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Fabric;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Microsoft.Diagnostics.EventFlow.ServiceFabric;
+using Microsoft.Diagnostics.EventFlow;
+
+namespace TestStatelessService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// This is the entry point of the service host process.
+        /// </summary>
+        private static void Main()
+        {
+            try
+            {
+                using (var diagnosticsPipeline = ServiceFabricDiagnosticPipelineFactory.CreatePipeline("eventFlowConfig.json"))
+                {
+                    ServiceRuntime.RegisterServiceAsync("TestStatelessServiceType", ctx => new TestStatelessService(ctx)).Wait();
+
+                    ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(TestStatelessService).Name);
+
+                    Thread.Sleep(Timeout.Infinite);
+                }
+            }
+            catch (Exception e)
+            {
+                ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+                throw;
+            }
+
+        }
+    }
+}
